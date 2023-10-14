4610bf06205faa0762157d648603a9d99c3d6909
JbanzCodingLabz <jacob.r.bananal@outlook.com>
2023-10-14 16:21:50 -0700

    FitnessApp.UI - Added FitnessBorder.cs, FitnessButton.cs, FitnessTextblock.cs

---
+++ FitnessApp.UI/Controls/FitnessButton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FitnessApp.UI.Controls
+{
+    public class FitnessButton : Button
+    {
+        static FitnessButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FitnessButton), new FrameworkPropertyMetadata(typeof(FitnessButton)));
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CornerRadius.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(FitnessButton), new PropertyMetadata(default(CornerRadius)));
+
+        public TextAlignment TextAlignment
+        {
+            get { return (TextAlignment)GetValue(TextAlignmentProperty); }
+            set { SetValue(TextAlignmentProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TextAlignment.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextAlignmentProperty =
+            DependencyProperty.Register(nameof(TextAlignment), typeof(TextAlignment), typeof(FitnessButton), new PropertyMetadata(default(TextAlignment)));
+
+
+    }
+}
---
+++ FitnessApp.UI/Controls/FitnessTextblock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FitnessApp.UI.Controls
+{
+    public class FitnessTextblock : TextBlock
+    {
+        static FitnessTextblock()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FitnessTextblock), new FrameworkPropertyMetadata(typeof(FitnessTextblock)));
+        }
+    }
+}
